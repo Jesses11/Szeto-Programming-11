f7004aa5f1109315eb490dfb604c831d25cddcf7
Jesses11 <71732560+Jesses11@users.noreply.github.com>
2021-08-15 00:22:30 -0700

    deleted a button

--- Game of Chance/src/sample/Controller.java
+++ Game of Chance/src/sample/Controller.java
@@ -15,26 +15,25 @@ public class Controller {
     public Label lblFinalScore;
     public Label lblRollNum;
     public Button btnEnd;
-    public Button btnSame;
     public Label lblTotalPoints;
     public double totalPoints = 0;
     int lastRoll;
 
 
     public void rollDice(ActionEvent actionEvent) {
-        int max = 1000;
+        int max = 6;
         int min = 1;
         int range = max - min + 1;
         lastRoll = (int) (Math.random() * range) + min;
         lblRollNum.setText(Integer.toString(lastRoll));
         btnRollDice.setDisable(true);
         btnHigher.setDisable(false);
-        btnSame.setDisable(false);
         btnLower.setDisable(false);
+        lblBetAmount.setDisable(false);
     }
 
     public void higher(ActionEvent actionEvent) {
-        int max = 1000;
+        int max = 6;
         int min = 1;
         int range = max - min + 1;
         int rand = (int) (Math.random() * range) + min;
@@ -53,12 +52,12 @@ public class Controller {
         btnRollDice.setDisable(false);
         btnHigher.setDisable(true);
         btnLower.setDisable(true);
-        btnSame.setDisable(true);
         lblBetAmount.clear();
+        lblBetAmount.setDisable(true);
     }
 
     public void lower(ActionEvent actionEvent) {
-        int max = 1000;
+        int max = 6;
         int min = 1;
         int range = max - min + 1;
         int rand = (int) (Math.random() * range) + min;
@@ -77,41 +76,16 @@ public class Controller {
         btnRollDice.setDisable(false);
         btnHigher.setDisable(true);
         btnLower.setDisable(true);
-        btnSame.setDisable(true);
         lblBetAmount.clear();
-    }
-
-    public void same(ActionEvent actionEvent) {
-        int max = 1000;
-        int min = 1;
-        int range = max - min + 1;
-        int rand = (int) (Math.random() * range) + min;
-        lblRollNum.setText(Integer.toString(rand));
-
-        if(rand==lastRoll){
-            double num = Double.parseDouble(lblBetAmount.getText());
-            totalPoints += num;
-            lblTotalPoints.setText("Total Points: " + totalPoints);
-        }
-        else{
-            double num = Double.parseDouble(lblBetAmount.getText());
-            totalPoints -= num;
-            lblTotalPoints.setText("Total Points: " + totalPoints);
-        }
-        btnRollDice.setDisable(false);
-        btnHigher.setDisable(true);
-        btnLower.setDisable(true);
-        btnSame.setDisable(true);
-        lblBetAmount.clear();
+        lblBetAmount.setDisable(true);
     }
 
     public void endGame(ActionEvent actionEvent) {
         lblFinalScore.setText("Final Score: " + totalPoints);
         btnLower.setDisable(true);
-        btnSame.setDisable(true);
         btnHigher.setDisable(true);
         btnRollDice.setDisable(true);
         btnEnd.setDisable(true);
-
+        lblBetAmount.setDisable(true);
     }
 }
